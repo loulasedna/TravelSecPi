7f6859d7f8584fb64919e882f16d0f9740b40ad9
loula sedna <loula.sedna@gmail.com>
2020-02-16 10:30:45 +0100

    ajout cahier des charges

--- backend.py
+++ backend.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "list of Endpoints"
 
-@app.route('/')
-
+#list file in directory
+@app.route('/list')
+def list():
+    pass
+    
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
