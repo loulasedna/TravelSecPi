f53e4cb19e2be7629f3d91b1ff08ac971a8e96cb
loula sedna <loula.sedna@gmail.com>
2020-05-03 18:50:29 +0200

    'scan wifi OK'

--- backend/network.py
+++ backend/network.py
@@ -39,7 +39,7 @@ class network(object):
         return self.interfaces
 
     def activate_interfaces(self, interface):
-        p = subprocess.run("sudo ip link set %s up".format(
+        p = subprocess.run("sudo ip link set {} up".format(
             interface), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return p.stdout.decode() if p.returncode == 0 else p.stderr.decode()
 
@@ -53,19 +53,65 @@ class network(object):
     def deactivate_nat(self, interface_in, interface_out):
         pass
 
-
 class wifi_interfaces(network):
 
-    def scan_wifi(self, wifi_interface):
-        # scan = subprocess.run("sudo iw %s scan".format(
-        #     wifi_interface),     shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # print(scan.returncode)
+    def scan_wifi(self, wifi_interfaces):
+        wifi_interfaces = list(wifi_interfaces)
+        scan = dict()
+        for wifi_interface in wifi_interfaces:
 
-        # if scan.returncode != 0:
-        #     raise Exception()
+            self.activate_interfaces(wifi_interface)
 
-        # print(type(scan))
-        pass
+            process = subprocess.run("sudo iw {} scan".format(
+                wifi_interface), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            if process.returncode != 0:
+                raise Exception()
+
+            item = None
+            lines = process.stdout.decode().splitlines()
+            for line in lines:
+
+                if re.search(r"[a-z0-9:]{17}", line):  # MAC/interface/BSS
+
+                    scan[item['interface']].append(
+                        item) if item is not None else None  # add dict only if created -> after
+
+                    item = dict()  # create new dict for new wifi
+
+                    item['mac'] = False
+                    item['wpa'] = False
+                    item['wpa2'] = False
+                    item['tkip'] = False
+                    item['ccmp'] = False
+                    item['psk'] = False
+
+                    split_line = line.split(' ')
+                    item['interface'] = split_line[2].split(')')[0]
+                    item['BSS'] = split_line[1].split('(')[0]
+                    if item['interface'] not in scan.keys():
+                        scan[item['interface']] = list()
+
+                if 'signal' in line:
+                    item['power'] = line.split(' ')[-2]
+                if 'freq' in line:
+                    item['freq'] = line.split(' ')[-1]
+                if 'SSID' in line:
+                    item['ssid'] = line.split(' ')[-1]
+                if 'WPA:' in line:
+                    item['wpa'] = True
+                if 'RSN:' in line:
+                    item['wpa2'] = True
+                if 'WEP:' in line:
+                    item['wep'] = True
+                if 'CCMP' in line:
+                    item['ccmp'] = True
+                if 'TKIP' in line:
+                    item['tkip'] = True
+                if 'PSK' in line:
+                    item['psk'] = True
+
+        return scan
 
     def connect_wifi(self, wifi_interface, password):
         pass
@@ -80,53 +126,6 @@ class ethernet_interfaces(network):
 
 
 if __name__ == "__main__":
-    # wifi = wifi_interfaces()
-    # print(wifi.get_interfaces())
-    # print(wifi.scan_wifi('wlan1'))
-    scan = dict()
-    item = None
-    with open('example/iwscan.txt', 'r') as text:
-        lines = text.readlines()
-
-        for line in lines:
-
-            if re.search(r"[a-z0-9:]{17}", line):  # MAC/interface/BSS
-
-                scan[item['interface']].append(
-                    item) if item is not None else None  # add dict only if created -> after 
-                
-                item = dict() # create new dict for new wifi
-                
-                item['mac'] = 'n'
-                item['wpa'] = 'n'
-                item['wpa2'] = 'n'
-                item['tkip'] = 'n'
-                item['ccmp'] = 'n'
-                item['psk'] = 'n'
-
-                split_line = line.split(' ')
-                item['interface'] = split_line[2].split(')')[0]
-                item['BSS'] = split_line[1].split('(')[0]
-                if item['interface'] not in scan.keys():
-                    scan[item['interface']] = list()
-
-            if 'signal' in line:
-                item['power'] = line.split(' ')[-2]
-            if 'freq' in line:
-                item['freq'] = line.split(' ')[-1].replace('\n', '')
-            if 'SSID' in line:
-                item['ssid'] = line.split(' ')[-1].replace('\n', '')
-            if 'WPA:' in line:
-                item['wpa'] = 'y'
-            if 'RSN:' in line:
-                item['wpa2'] = 'y'
-            if 'WEP:' in line:
-                item['wep'] = 'y'
-            if 'CCMP' in line:
-                item['ccmp'] = 'y'
-            if 'TKIP' in line:
-                item['tkip'] = 'y'
-            if 'PSK' in line:
-                item['psk'] = 'y'
-        
-        print (scan)
+    wifi = wifi_interfaces()
+    print(wifi.get_interfaces())
+    print(wifi.scan_wifi(['wlan0', 'wlan1']))
